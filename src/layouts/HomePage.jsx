d7e61508ff7f481255cfb9636c73654d44c5ede7
CuTrung <cuphanthanhtrung4112003@gmail.com>
2024-06-15 14:49:01 +0700

    chore: off check perrmission

--- src/layouts/HomePage.jsx
+++ src/layouts/HomePage.jsx
@@ -42,7 +42,8 @@ const HomePage = () => {
               style={{
                 padding: 12,
                 minHeight: '100vh',
-              }}>
+              }}
+            >
               {outlet}
             </div>
           </Content>
